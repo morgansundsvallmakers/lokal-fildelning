63a6192f082c5be629932861388524c32d6e0c67
morgansundsvallmakers <morgan@sundsvallmakers.se>
2026-09-08 22:12:26 +0200

    Use PNG-compatible Windows resource builder

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,8 +1,11 @@
 @echo off
 setlocal
 
+if exist resource.syso del /q resource.syso
+if exist rsrc_windows_amd64.syso del /q rsrc_windows_amd64.syso
+
 echo Skapar Windows-resurs med programikonen...
-go run github.com/akavel/rsrc@latest -arch amd64 -ico assets\icon.ico -o resource.syso
+go run github.com/tc-hib/go-winres@latest simply --arch amd64 --icon assets\tray-icon.png --manifest gui --file-description "Lokal fildelning" --product-name "Lokal fildelning" --original-filename "lokal-fildelning.exe"
 if errorlevel 1 exit /b 1
 
 echo Bygger lokal-fildelning.exe...
